9ea8f7b9982be1be18f53fd0b089c9087de4fe83
Ian <iangwhite96@gmail.com>
2022-09-02 22:24:15 -0500

    Bullets destroy gameobjects from "killable" layer

--- Assets/BulletBase.cs
+++ Assets/BulletBase.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class BulletBase : MonoBehaviour
 {
-    Rigidbody rb;
+    Rigidbody rb = null;
     [SerializeField] protected float travelSpeed = .25f;
+    //[SerializeField] LayerMask Killable;
 
     private void Awake()
     {
+        Debug.Log("Bullet Spawn!");
         rb = GetComponent<Rigidbody>();
     }
 
@@ -21,5 +23,18 @@ public class BulletBase : MonoBehaviour
     {
         Vector3 moveOffset = transform.forward * travelSpeed;
         rb.MovePosition(rb.position + moveOffset);
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Bullet Collision!");
+        //TODO APPLY DAMAGE HERE
+      
+        if(col.gameObject.layer == LayerMask.NameToLayer("Killable"))
+        {
+            Destroy(col.gameObject); 
+        }
+        
+        gameObject.SetActive(false);
     }
 }

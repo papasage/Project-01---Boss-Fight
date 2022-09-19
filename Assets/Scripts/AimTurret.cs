723bba907038c1f701a957513b356cf4476a4024
Ian Guglielmi-White <ianrgwhite@gmail.com>
2022-09-18 20:34:03 -0500

    Mouse Aim Works!

--- Assets/Scripts/AimTurret.cs
+++ Assets/Scripts/AimTurret.cs
@@ -10,32 +10,37 @@ public class AimTurret : MonoBehaviour
     [SerializeField] float mousePosZ = 100f;
     Ray ray;
     Vector3 mousePos;
+    RaycastHit hit;
     //While Yellow and Green are pointing in the same location, they are hitting the floor in different locations because the mousePos is not limited on the y transform
 
     void Update()
     {
         RaycastMouse();
-        TurnTurret();
 
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Debug.DrawRay(cannonAimer.position, forward, Color.red);                                    //RED Raycast is the Tank's "Laser Sight"
-        Debug.DrawRay(cam.transform.position, mousePos - cam.transform.position, Color.green);      //GREEN Raycast is Pointing from camera plane to the mouse 
+        
     }
-    void TurnTurret()
+    void TurnTurret(Vector3 _hit)
     {
-        Vector3 dir = new Vector3(mousePos.x, cannonAimer.position.y, mousePos.z);
-        Debug.DrawRay(cannonAimer.position, dir, Color.yellow);                                     //YELLOW Raycast is from Cannon's tip to mouse
+        Vector3 dir = new Vector3(_hit.x, cannonAimer.position.y, _hit.z);
+
+        Debug.DrawRay(cam.transform.position, dir - cam.transform.position, Color.green);      //GREEN Raycast is Pointing from camera plane to the mouse 
+
         rbTurret.transform.LookAt(dir);
     }
 
     void RaycastMouse()
     {
+        
+        ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        
+        if(Physics.Raycast(ray, out hit))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            TurnTurret(hit.point);
+        }
 
-        //Physics.Raycast
 
-        mousePos = Input.mousePosition;
-        mousePos.z = mousePosZ;
-        mousePos = cam.ScreenToWorldPoint(mousePos);
     }
 
 
